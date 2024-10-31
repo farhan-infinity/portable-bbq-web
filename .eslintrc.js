bf91b6d407e0dbadf2c45cb836cb8b6ce6218aac
farhan-infinty <development.bits@gmail.com>
2024-10-31 20:32:15 +0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended", // Add this line
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser", // Add this line
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier", "@typescript-eslint"], // Add @typescript-eslint plugin
+  rules: {
+    "prettier/prettier": "error",
+    // "no-debugger": "off", // Allow debugger statements
+    // Add any additional rules you want to customize here
+  },
+  settings: {
+    react: {
+      version: "detect", // Automatically picks the version you have installed
+    },
+  },
+};
